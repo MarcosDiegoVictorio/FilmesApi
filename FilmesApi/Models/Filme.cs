31c830569f1b965469cda89e4e6c6c7df420dd9c
Marcos Diego <marcosdiego159@gmail.com>
2024-01-23 22:18:17 -0300

    Adicionado o controle de inserção na classe Filme

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -1,8 +1,17 @@
-﻿namespace FilmesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
 
 public class Filme
 {
+    [Required(ErrorMessage = "O Titulo do Filme é Obrigatorio")]
     public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O Genero do Filme é Obrigatorio")]
+    [MaxLength(50, ErrorMessage ="O tamanho do genero não pode exceder 50 caracteres")]
     public string Genero { get; set; }
+
+    [Required]
+    [Range(70,600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
     public int Duracao { get; set; }
 }

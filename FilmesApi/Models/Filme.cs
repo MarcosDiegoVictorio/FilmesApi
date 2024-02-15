06c9db9f23c80a9eac4240f2df2138e40134aefc
Marcos Diego <marcosdiego159@gmail.com>
2024-02-14 22:33:11 -0300

    Added MySql DataBase Connection

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -4,14 +4,18 @@ namespace FilmesApi.Models;
 
 public class Filme
 {
-    [Required(ErrorMessage = "O Titulo do Filme é Obrigatorio")]
+    [Key]
+    [Required]
+    public int Id { get; internal set; }
+
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
     public string Titulo { get; set; }
 
-    [Required(ErrorMessage = "O Genero do Filme é Obrigatorio")]
-    [MaxLength(50, ErrorMessage ="O tamanho do genero não pode exceder 50 caracteres")]
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
     public string Genero { get; set; }
 
     [Required]
-    [Range(70,600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
+    [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
     public int Duracao { get; set; }
 }
